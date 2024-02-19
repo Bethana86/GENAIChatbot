a00ddd79ebeaf82ba4d21801ddf3cecfa674ffde
Bethanasamy Rajamani <46414051+Bethana86@users.noreply.github.com>
2024-02-19 09:58:53 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ if "chat_session" not in st.session_state:
 
 
 # Display the chatbot's title on the page
-st.title("🤖 MY AI ASSITANT - ChatBot")
+st.title("🤖 MY AI ASSISSTANT - ChatBot")
 
 # Display the chat history
 for message in st.session_state.chat_session.history:
