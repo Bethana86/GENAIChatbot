ae10b4b3a97aa01a03fba5032f6591daa5761588
Bethanasamy Rajamani <46414051+Bethana86@users.noreply.github.com>
2024-05-12 11:49:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 # Configure Streamlit page settings
 st.set_page_config(
-    page_title="ALGOSTATS GenAI!",
+    page_title="GenAI Chatbot!",
     page_icon=":brain:",  # Favicon emoji
     layout="centered",  # Page layout option
 )
